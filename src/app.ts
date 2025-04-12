4980be2fef25efde37627aab27b2970a1378bb5d
madisonrubylee <lamantciel@naver.com>
2025-04-12 13:13:25 +0900

    fix : 최초에 음료 렌더링 안되는 이슈 수정

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import { VendingMachine } from "./classes/VendingMachine";
 import { initializeApp } from "./handlers/init";
+import { inventoryStatus } from "./ui/inventoryStatus";
 
 const vm = new VendingMachine();
 
 document.addEventListener("DOMContentLoaded", () => {
+  // 초기 음료수 목록 표시
+  inventoryStatus(vm);
+  // 나머지 초기화
   initializeApp(vm);
 });
